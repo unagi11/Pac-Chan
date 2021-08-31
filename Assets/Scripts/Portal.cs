9c50df8eab43691458a0a5dee69751341e4ffa65
Unagi11 <unagi11@naver.com>
2021-08-31 21:31:10 +0900

    포탈 제작

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Portal Out;
+
+    public bool isOuting;
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.CompareTag("Player") && isOuting is false)
+        {
+            Out.isOuting = true;
+            other.transform.position = Out.transform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        
+        if (other.CompareTag("Player") && isOuting)
+        {
+            isOuting = false;
+        }
+        
+    }
+}
